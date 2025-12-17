f633bf6226e6aee4dfe8efbddf4381b3589410bc
Girish <girish@Mac.lan>
2025-12-17 18:37:14 +0530

    updated the ecs task

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,56 +1,3 @@
-resource "aws_ecs_cluster" "ecs" {
-  name = "ecs-demo-cluster"
-}
-
-resource "aws_ecs_task_definition" "task" {
-  family                   = "multi-platform-task"
-  requires_compatibilities = ["FARGATE"]
-  network_mode             = "awsvpc"
-  cpu                      = 256
-  memory                   = 512
-  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
-
-  container_definitions = jsonencode([
-    {
-      name  = "web-app"
-      image = "${aws_ecr_repository.app.repository_url}:latest"
-      portMappings = [{
-        containerPort = 8080
-      }]
-      environment = [{
-        name  = "PLATFORM"
-        value = "ECS-Fargate"
-      }]
-    }
-  ])
-}
-resource "aws_ecs_task_definition" "multi_platform_task" {
-  family                   = "multi-platform-task"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"
-  memory                   = "512"
-
-  container_definitions = jsonencode([
-    {
-      name      = "web-app"
-      image     = "123456789012.dkr.ecr.us-east-1.amazonaws.com/multi-platform-app:latest"
-      portMappings = [
-        {
-          containerPort = 8080
-          protocol      = "tcp"
-        }
-      ]
-      environment = [
-        {
-          name  = "PLATFORM"
-          value = "ECS-Fargate"
-        }
-      ]
-    }
-  ])
-}
-
 resource "aws_ecs_service" "ecs_web_service" {
   name            = "ecs-web-service"
   cluster         = aws_ecs_cluster.ecs.id
@@ -59,15 +6,18 @@ resource "aws_ecs_service" "ecs_web_service" {
   launch_type     = "FARGATE"
 
   network_configuration {
-    subnets         = ["subnet-xxxx", "subnet-yyyy"]
-    security_groups = ["sg-xxxx"]
+    # Dynamically use the subnets from your eks.tf data source
+    subnets          = data.aws_subnets.eks.ids
+    # You should reference a security group resource here
+    security_groups  = [aws_security_group.ecs_sg.id] 
     assign_public_ip = true
   }
 
   load_balancer {
-    target_group_arn = "arn:aws:elasticloadbalancing:region:account-id:targetgroup/xxxx"
+    # DO NOT use the string "arn:aws:elasticloadbalancing:region..."
+    # Reference your actual target group resource name here:
+    target_group_arn = aws_lb_target_group.app_tg.arn 
     container_name   = "web-app"
     container_port   = 8080
   }
 }
-
