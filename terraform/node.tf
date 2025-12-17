5272ccab8819391fbb017974594af6e6598d4270
Girish <girish@Mac.lan>
2025-12-17 18:07:24 +0530

    updated the node group

---
+++ terraform/node.tf
@@ -0,0 +1,20 @@
+resource "aws_eks_node_group" "nodes" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "eks-node-group"
+  node_role_arn   = aws_iam_role.eks_nodes.arn # Matches your IAM file
+  subnet_ids      = data.aws_subnets.eks.ids
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  instance_types = ["t3.medium"]
+
+  depends_on = [
+    aws_iam_role_policy_attachment.amazon_eks_worker_node_policy,
+    aws_iam_role_policy_attachment.amazon_eks_cni_policy,
+    aws_iam_role_policy_attachment.amazon_ec2_container_registry_read_only,
+  ]
+}
