81e49c0b2abda9eb55edc5ae0efe517080b148d2
Girish <girish@Mac.lan>
2025-12-17 16:07:55 +0530

    Updated the ecr

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,14 +1,11 @@
 resource "aws_ecr_repository" "app" {
-  name                 = "multi-platform-app"
-  image_tag_mutability = "MUTABLE"
+  name = "multi-platform-app"
 
   image_scanning_configuration {
     scan_on_push = true
   }
-
-  tags = {
-    Project = "multi-orchestrator"
-    Env     = "dev"
-  }
 }
 
+output "ecr_repo_url" {
+  value = aws_ecr_repository.app.repository_url
+}
--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,15 +1,35 @@
+# Get supported availability zones
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+# Pick first 2 supported AZs
+locals {
+  eks_azs = slice(data.aws_availability_zones.available.names, 0, 2)
+}
+
+# Fetch default subnets ONLY from selected AZs
+data "aws_subnets" "eks" {
+  filter {
+    name   = "availability-zone"
+    values = local.eks_azs
+  }
+
+  filter {
+    name   = "default-for-az"
+    values = ["true"]
+  }
+}
+
 resource "aws_eks_cluster" "eks" {
   name     = "eks-demo-cluster"
   role_arn = aws_iam_role.eks_role.arn
 
   vpc_config {
-    subnet_ids = data.aws_subnets.default.ids
+    subnet_ids = data.aws_subnets.eks.ids
   }
-}
 
-data "aws_subnets" "default" {
-  filter {
-    name   = "default-for-az"
-    values = ["true"]
-  }
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_policy
+  ]
 }
