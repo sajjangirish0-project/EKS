db379426e4ca936798d780bf4bd5d16f01117992
Girish <girish@Mac.lan>
2025-12-17 18:25:57 +0530

    updated the ecs task

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -24,3 +24,50 @@ resource "aws_ecs_task_definition" "task" {
     }
   ])
 }
+resource "aws_ecs_task_definition" "multi_platform_task" {
+  family                   = "multi-platform-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([
+    {
+      name      = "web-app"
+      image     = "123456789012.dkr.ecr.us-east-1.amazonaws.com/multi-platform-app:latest"
+      portMappings = [
+        {
+          containerPort = 8080
+          protocol      = "tcp"
+        }
+      ]
+      environment = [
+        {
+          name  = "PLATFORM"
+          value = "ECS-Fargate"
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "ecs_web_service" {
+  name            = "ecs-web-service"
+  cluster         = aws_ecs_cluster.ecs.id
+  task_definition = aws_ecs_task_definition.multi_platform_task.arn
+  desired_count   = 2
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = ["subnet-xxxx", "subnet-yyyy"]
+    security_groups = ["sg-xxxx"]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = "arn:aws:elasticloadbalancing:region:account-id:targetgroup/xxxx"
+    container_name   = "web-app"
+    container_port   = 8080
+  }
+}
+
